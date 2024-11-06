68c548bd53414c764ff629a33202a3b2e05fc3d1
Mamkasomnoy <nikita12345914@gmail.com>
2024-11-06 16:24:37 +0700

    Update Переменные.py

--- Переменные.py
+++ Переменные.py
@@ -1,10 +1,10 @@
-number_of_completed_homework_assignments = 12
-print("number of completed homework assignments:", number_of_completed_homework_assignments, "pieces")
-NumberOfHoursSpent = 1.5
-print("Number of hours spent", NumberOfHoursSpent, "h")
+Number_of_completed_homework_assignments = 12
+print("Number of completed homework assignments:", Number_of_completed_homework_assignments, "pieces")
+Number_of_hours_spent = 1.5
+print("Number of hours spent", Number_of_hours_spent, "h")
 Course_name = "Python"
 print(Course_name)
-Time_for_one_task = NumberOfHoursSpent / number_of_completed_homework_assignments
+Time_for_one_task = Number_of_hours_spent / Number_of_completed_homework_assignments
 print("Time for one task", Time_for_one_task, "h")
-print("Course name:", Course_name, "Number of completed homework assignments:", number_of_completed_homework_assignments, "pieces", "Number of hours spent:", NumberOfHoursSpent, "h", "Time for one task:", Time_for_one_task, "h")
-print("Название курса:", Course_name, "Количество выполненных домашних заданий:", number_of_completed_homework_assignments, "штук", "Количество затраченных часов:", NumberOfHoursSpent, "ч", "Время на выполнение одной задачи:", Time_for_one_task, "ч")
+print("Course name:", Course_name, "Number of completed homework assignments:", Number_of_completed_homework_assignments, "pieces", "Number of hours spent:", Number_of_hours_spent, "h", "Time for one task:", Time_for_one_task, "h")
+print("Название курса:", Course_name, "Количество выполненных домашних заданий:", Number_of_completed_homework_assignments, "штук", "Количество затраченных часов:", Number_of_hours_spent, "ч", "Время на выполнение одной задачи:", Time_for_one_task, "ч")
