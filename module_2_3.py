361f3a870179388c6c06930f9a2d62c71308ce1a
Mamkasomnoy <nikita12345914@gmail.com>
2024-11-16 23:30:44 +0700

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -2,7 +2,9 @@ my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 a = 0
 while len(my_list) > a:
     if int(my_list[a]) > 0:
-        print(int(my_list[a]))
+        print(my_list[a])
         a += 1
-    else: 
+    elif my_list[a] == -5:
+        break
+    else:
         a += 1
