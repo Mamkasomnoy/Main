8eeefd2f0c0e634eea476d5bea6f2c9d92608adf
Mamkasomnoy <nikita12345914@gmail.com>
2024-11-23 23:08:35 +0700

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -6,16 +6,26 @@ the_address_is_correct = 0
 sender_one = "university.help@gmail.com"
 def send_email(message, recipient, sender="university.help@gmail.com"):
     while 1 > 0:
-        if (str(dog) in str(recipient) and str(com) in str(recipient) or str(dog) in str(recipient) and str(ru) in str(recipient) or str(dog) in str(recipient) and str(net) in str(recipient)) and (str(dog) in str(sender) and str(com) in str(sender) or str(dog) in str(sender) and str(ru) in str(sender) or str(dog) in str(sender) and str(net) in str(sender)):
+        str(sender)
+        str(recipient)
+        if (
+                (str(dog) in recipient and str(com) in recipient or str(dog) in recipient and str(ru) in recipient or str(dog) in recipient and str(net) in recipient)
+                and (str(dog) in sender and str(com) in sender or str(dog) in sender and str(ru) in sender or str(dog) in sender and str(net) in sender)
+                and (recipient.endswith(com) or recipient.endswith(ru) or recipient.endswith(net)) and (sender.endswith(com) or sender.endswith(ru) or sender.endswith(net))
+            ):
+
             if recipient == sender:
                 print("Нельзя отправить письмо самому себе!")
                 break
+
             elif sender_one == sender:
                 print('Письмо успешно отправлено с адреса:', sender, 'на адрес:', recipient)
                 break
+
             else:
                 print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса:', sender, 'на адрес:', recipient)
                 break
+
         else:
             print("Невозможно отправить письмо с адреса:", sender, " на адрес:", recipient)
             break
